8fda13c3ca0a4b672ca94ada3c6b55d8e6b7370d
MrDigger <mrdigger@mail.ru>
2019-06-18 23:52:06 +0300

    Documentation has been added.

---
+++ docs/api.js
@@ -0,0 +1,24 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "ImageListComponent",
+        "ImageListService",
+        "PopupComponent",
+        "PopupService"
+    ],
+    "modules": [
+        "ImageListModule"
+    ],
+    "allModules": [
+        {
+            "displayName": "ImageListModule",
+            "name": "ImageListModule",
+            "description": "Image list component"
+        }
+    ],
+    "elements": [
+        "ImageList",
+        "PopupComponent"
+    ]
+} };
+});
